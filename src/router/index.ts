663ff41a2e75c92010140e92632edb1a8f88dc1b
Mayank-sha <officialmayankks@gmail.com>
2023-06-22 17:50:14 +0530

    created user login screen

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,22 +1,28 @@
-import { createRouter, createWebHistory } from '@ionic/vue-router';
-import { RouteRecordRaw } from 'vue-router';
-import HomePage from '../views/HomePage.vue';
-import Conversation from '../views/conversation/Conversation.vue';
+import { createRouter, createWebHistory } from "@ionic/vue-router";
+import { RouteRecordRaw } from "vue-router";
+import HomePage from "../views/HomePage.vue";
+import Conversation from "../views/conversation/Conversation.vue";
+import UserLogin from "../views/User/UserLogin.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
-    path: '/',
-    redirect: '/home',
+    path: "/",
+    redirect: "/home",
   },
   {
-    path: '/home',
-    name: 'Home',
+    path: "/home",
+    name: "Home",
     component: HomePage,
   },
   {
-    path: '/conversation',
-    name: 'conversation',
+    path: "/conversation",
+    name: "conversation",
     component: Conversation,
+  },
+  {
+    path: "/login",
+    name: "login",
+    component: UserLogin,
   },
 ];
 
